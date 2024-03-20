92db89f679e47440b731192c3ac2dd39ff3d4268
ceyx <fm278841@gmail.com>
2024-03-19 18:33:00 -0700

    test

--- servo.py
+++ servo.py
@@ -1,12 +1,13 @@
 import math
 import pigpio
+import time
 
 class Servo:
     def __init__(self, io, pin):
         self.io = io
         self.pin = pin
         self.angle = 0
-        self.bebug = False
+        self.debug = False
 
         self.io.set_mode(self.pin, pigpio.OUTPUT)
         self.io.set_PWM_range(self.pin, 255)
@@ -23,10 +24,19 @@ class Servo:
     def powerOff(self):
         if self.debug:
             print("Powering off")
-        self.setAngle(0):
+        self.setAngle(0)
 
 
 if __name__ == "__main__":
     import pigpio
+    import time
+    import sys
     io = pigpio.pi()
-    servo1 = Servo(io)
+    servo1 = Servo(io, 19)
+    if not io.connected:
+        print("unable to connect")    
+    while True:
+        for i in range(360):
+            servo1.setAngle(i)
+            print(i)
+            time.sleep(0.1)
